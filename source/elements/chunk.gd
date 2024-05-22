211dd554c691e7c051c58cd34f166811586ee466
LidesSam <lides.sam@gmail.com>
2024-05-21 23:37:57 -0300

    new freature: map gen improved freelocs and foes moved to chunk
    this allow to modify freelocs in real time, add new encounters for example

--- source/elements/chunk.gd
+++ source/elements/chunk.gd
@@ -1,7 +1,8 @@
 extends Node2D
-
+#chunk.gd
 
 var mapLocs=[]
+var freeLocs=[]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,8 +13,16 @@ func _process(delta):
 	pass
 	
 func add_location(loc):
-	
 	add_child(loc)
 	mapLocs.push_back(loc)
+	if(mapLocs.size()>1):
+		freeLocs.push_back(loc)
 	
-	
+func add_rand_foe_encounters( min:int=1, max:int=2):
+	if(max<=min):
+		max= min+1
+	var r = randi()%floor(max-min)+min
+	for i in range(r):
+		freeLocs.shuffle()
+		var loc =  freeLocs.pop_front()
+		loc.set_foe()
--- source/factory/mapGen.gd
+++ source/factory/mapGen.gd
@@ -5,6 +5,7 @@ var locTemp= preload("res://source/elements/location.tscn")
 #generate small map
 var startLoc
 var startChunk
+var step = 64
 
 func gen_linear_map(min=5, max=10):
 	#fist define the ssize of the map
@@ -14,28 +15,22 @@ func gen_linear_map(min=5, max=10):
 	var  chunk=load("res://source/elements/chunk.tscn").instantiate()
 	var starChunk=chunk
 	var lastLoc=null
-	var freeLocs=[]
 	
 	for i in range(chunksize):
 		var loc=locTemp.instantiate()
 		if(lastLoc!=null):
 			lastLoc.add_conection(loc)
 			loc.add_conection(lastLoc)
-			freeLocs.push_back(loc)
-			
 		lastLoc=loc
 		
-		loc.position=Vector2(0,0-64*i)
+		loc.position=Vector2(0,0-step*i)
 		chunk.add_location(loc)
-		
-	for i in range(2):
-		freeLocs.shuffle()
-		var loc =  freeLocs.pop_front()
-		loc.set_foe()
+	chunk.add_rand_foe_encounters(2,4)
+	
 		
 	#END GAME LOCK
 	var endloc=locTemp.instantiate()
-	endloc.position=lastLoc.position-Vector2(0,64);
+	endloc.position=lastLoc.position-Vector2(0,step);
 	endloc.set_roadend()
 	lastLoc.add_conection(endloc)
 	endloc.add_conection(lastLoc)
